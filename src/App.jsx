bf215f48d9bd7d800fe39c9a3f20636e67392fdc
Hamsa777 <hamsalif52@gmail.com>
2025-04-24 19:02:02 +0200

    Routing aktiviert für / und /danke

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import KIPartnerLanding from "./KIPartnerLanding.jsx";
+import DankeSeite from "./pages/danke.jsx"; // bleibt in /src/pages
 
 function App() {
-  return <KIPartnerLanding />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<KIPartnerLanding />} />
+        <Route path="/danke" element={<DankeSeite />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
